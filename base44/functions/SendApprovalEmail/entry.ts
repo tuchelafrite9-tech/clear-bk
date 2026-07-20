01063db9299ff850377589d0273bdc033b6a295c
Tuche Lafrite <tuchelafrite9@gmail.com>
2026-07-20 19:07:49 +0000

    File changes

--- base44/functions/SendApprovalEmail/entry.ts
+++ base44/functions/SendApprovalEmail/entry.ts
@@ -1,7 +1,7 @@
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.38';
 
 const RESEND_API_KEY = Deno.env.get("RESEND_API_KEY");
-const FROM_EMAIL = "ClearBank No-Reply <info@clearbank.fr>";
+const FROM_EMAIL = "ClearBank No-Reply <info@bk-clear.com>";
 
 Deno.serve(async (req) => {
   try {
